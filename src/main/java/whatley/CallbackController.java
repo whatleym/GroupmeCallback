c8cf492be46d3bebf507d6d20a7a4e1b7130ccea
Matt Whatley <whatleym@uga.edu>
2016-08-29 21:10:56 -0400

    ...

--- src/main/java/whatley/CallbackController.java
+++ src/main/java/whatley/CallbackController.java
@@ -1,3 +1,5 @@
+package whatley;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
