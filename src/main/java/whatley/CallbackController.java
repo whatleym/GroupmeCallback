4d166d2db8753bcbd82092e636a268b1994a0728
Matt Whatley <whatleym@uga.edu>
2016-08-30 02:25:48 -0400

    Testing sleep

--- src/main/java/whatley/CallbackController.java
+++ src/main/java/whatley/CallbackController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.lang.*;
 
 @RestController
 public class CallbackController{
@@ -11,6 +12,11 @@ public class CallbackController{
 	@RequestMapping(value = "/callback", method = RequestMethod.POST)
 	public void callback(@RequestBody String text){
 		System.out.println(text);
+		try{	
+			Thread.sleep(5000);
+		}catch(Exception e){
+			System.out.println(e);
+		}
 	}
 
 }
