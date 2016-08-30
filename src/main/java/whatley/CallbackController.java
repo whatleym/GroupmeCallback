200cffea2b8a78b4fea77c891790de2edc51e41e
Matt Whatley <whatleym@uga.edu>
2016-08-29 20:46:26 -0400

    ...

--- src/main/java/whatley/CallbackController.java
+++ src/main/java/whatley/CallbackController.java
@@ -1,5 +1,3 @@
-package whatley;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,14 +8,8 @@ public class CallbackController{
 
 	@RequestMapping(value = "/callback", method = RequestMethod.POST)
 	public void callback(@RequestBody String text){
-
-		//Callback newCallback = new Callback(response, meta);
-
-		//System.out.println("\n\nThis is the text: " + newCallback.toString() + "\n\n");
-
-		System.out.println(text);
-
-		//return newCallback;
+		System.out.println("\n");
+		System.out.println(text + "\n");
 	}
 
 }
