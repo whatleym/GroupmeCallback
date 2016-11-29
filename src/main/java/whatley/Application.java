b7018582d70b2f40731e0ae66aea32d2d7b0c285
Matt Whatley <whatleym@uga.edu>
2016-11-29 01:51:29 -0500

    Added testing print

--- src/main/java/whatley/Application.java
+++ src/main/java/whatley/Application.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application{
 		public static void main(String[] args){
+			System.out.println("Running NOW");
 			SpringApplication.run(Application.class, args);
+			System.out.println("Reached end of main... idk what happens now.");
 		}
 
 		public static void printThis(String string){
